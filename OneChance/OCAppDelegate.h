ff0257c1d15ae36668aea5c67c6e22074ec514b0
Jon Como <jon@underground.net>
2014-03-11 01:53:06 -0400

    Initial Commit

---
+++ OneChance/OCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OCAppDelegate.h
+//  OneChance
+//
+//  Created by Jon Como on 3/11/14.
+//  Copyright (c) 2014 Jon Como. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OneChance/OCMyScene.h
@@ -0,0 +1,13 @@
+//
+//  OCMyScene.h
+//  OneChance
+//
+
+//  Copyright (c) 2014 Jon Como. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface OCMyScene : SKScene
+
+@end
